03390ba08cd836daf4faf8dd09cccbf38fe0e483
art0ch <71591850+art0ch@users.noreply.github.com>
2021-01-05 04:34:37 +0300

    Now both fragments can save their values (with hide/show methods). Refactored the code a little and deleted some XMLs aren't used here.

--- app/src/main/java/com/will/android/faurecia/MainActivity.java
+++ app/src/main/java/com/will/android/faurecia/MainActivity.java
@@ -2,53 +2,62 @@ package com.will.android.faurecia;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     public static Form mForm = new Form();
+    private final PpmFragment mPpmFragment = new PpmFragment();
+    private final DleFragment mDleFragment = new DleFragment();
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
         BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
         bottomNav.setOnNavigationItemSelectedListener(navListener);
 
-        Fragment startFrag = new PpmFragment();
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                startFrag).commit();
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container_2, mDleFragment)
+                .replace(R.id.fragment_container, mPpmFragment)
+                .hide(mDleFragment)
+                .show(mPpmFragment)
+                .commit();
 
     }
-
 
     private final BottomNavigationView.OnNavigationItemSelectedListener navListener =
             new BottomNavigationView.OnNavigationItemSelectedListener() {
                 @Override
                 public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                    Fragment selectedFragment = null;
+
+                    FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                    ft.setCustomAnimations(android.R.animator.fade_in,
+                            android.R.animator.fade_out);
 
                     if (item.getItemId() == R.id.nav_PPM) {
-                        selectedFragment = new PpmFragment();
+                        ft.hide(mDleFragment)
+                                .show(mPpmFragment)
+                                .commit();
+
                     } else if (item.getItemId() == R.id.nav_DLE) {
-                        selectedFragment = new DleFragment();
+                        ft.hide(mPpmFragment)
+                                .show(mDleFragment)
+                                .commit();
                     }
-
-                    if (selectedFragment != null) {
-                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                                selectedFragment).commit();
-                    }
-
                     return true;
                 }
             };
 }
+
